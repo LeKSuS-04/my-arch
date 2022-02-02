85b447f4a8e6949edad603df7244bd887ff7ffb1
Alexey Tarasov <tarasovion2004@gmail.com>
2022-02-02 17:30:55 +0000

    Added some packages

--- packages.sh
+++ packages.sh
@@ -12,23 +12,24 @@ sudo pacman -Syyu --noconfirm
 
 PACMAN_PKGS=(
     # ===== Screen =====
-    'brightnessctl'     # Brightness controller
+    'brightnessctl'         # Brightness controller
 
     # ===== Audio =====
-    'pulseaudio'        # Sound server
-    'pulseaudio-alsa'   # Pulseaudio to manage ALSA
+    'alsa-utils'            # Utilities for better sound control
+    'pulseaudio'            # Sound server
+    'pulseaudio-alsa'       # Pulseaudio to manage ALSA
 
     # ===== General purpose =====
-    'alacritty'         # Terminal emulator
-    'ranger'            # File manager
-    'rofi'              # Window switcher & application launcher
+    'alacritty'             # Terminal emulator
+    'ranger'                # File manager
+    'rofi'                  # Window switcher & application launcher
 )
 for PKG in "${PACMAN_PKGS[@]}"; do
     echo
     echo "Installing ${PKG} [pacman]"
     sudo pacman -S "${PKG}" --noconfirm --needed
 done
-echo 
+echo
 echo "Pacman packages are installed :)"
 
 
@@ -41,19 +42,23 @@ cd yay
 makepkg -si
 
 AUR_PKGS=(
+    # ===== Screen =====
+    'picom-jonaburg-git'    # Picom fork with extra functionality
+
     # ===== Audio =====
-    'pacmixer'          # CLI audio mixer
+    'pacmixer'              # CLI audio mixer
 
     # ===== Graphics =====
-    'aseprite'          # Pixel-art editor
+    'aseprite'              # Pixel-art editor
 
     # ===== Misc CLI tools =====
-    'ufetch'            # Fetching system information
-    'unimatrix'         # Scipt to simulate terminal from "The Matrix"
+    'ufetch'                # Fetching system information
+    'unimatrix'             # Scipt to simulate terminal from "The Matrix"
 
     # ===== General purpose =====
-    'mons'              # Utility for dual-monitor control
-    'spotify'           # Music player
+    'mons'                      # Utility for dual-monitor control
+    'nerd-fonts-complete'       # Font pack
+    'spotify'                   # Music player
     'visual-studio-code-bin'    # Superiour text editor
 )
 for PKG in "${AUR_PKGS[@]}"; do
