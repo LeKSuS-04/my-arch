acb70b1ac211a69cf277b6503bfc9e6dc9d66c29
LeKSuS-04 <tarasovion2004@gmail.com>
2022-06-13 22:18:05 +0300

    Open power menu when clicked on power icon in bar

--- eww/scripts/eww_toggle.sh
+++ eww/scripts/eww_toggle.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-windows=$(eww windows)
+windows=$(~/.local/bin/eww windows)
 
 while IFS= read -r line; do
     if [[ $line == "$1" ]]; then
-        eww open $1
+        ~/.local/bin/eww open "$1"
     elif [[ $line == "*${1}" ]]; then
-        eww close $1
+        ~/.local/bin/eww close "$1"
     fi
 done <<< "$windows"
